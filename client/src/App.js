4d920cdde6fb547e626f39fa7ec2ef9d012d48f5
Agnes Chueng <agneschueng@gmail.com>
2020-11-25 17:44:50 -0600

    include route /about for about page

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Navbar/>
       <Wrapper>
         <Route exact path="/" component={About}/>
+        <Route exact path="/about" component={About}/>
         <Route exact path="/portfolio" component={Portfolio}/>
         <Route exact path="/contact" component={Contact}/>
       </Wrapper>
