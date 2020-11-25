87f76a618dbb2de72761db56e182c71a7ad72640
Agnes Chueng <agneschueng@gmail.com>
2020-11-25 17:45:14 -0600

    add className active if on the page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,19 @@
 function Navbar() {
     return (
         <Navbar bg="light" expand="lg" fixed="top">
-        <Navbar.Brand href="#home">Agnes Chueng</Navbar.Brand>
+        <Navbar.Brand to="/">Agnes Chueng</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-                <Nav.Link to="/">About</Nav.Link>
-                <Nav.Link to="/portfolio">Portfolio</Nav.Link>
-                <Nav.Link to="/contact">Contact</Nav.Link>
+                <Nav.Link to="/about"
+                className={
+                    window.location.pathname === "/" || 
+                    window.location.pathname === "/about"
+                      ? "active" : ""
+                }
+                >About</Nav.Link>
+                <Nav.Link to="/portfolio" className={window.location.pathname === "/portfolio" ? "active" : ""}>Portfolio</Nav.Link>
+                <Nav.Link to="/contact" className={window.location.pathname === "/contact" ? "active" : ""}>Contact</Nav.Link>
             </Nav>
         </Navbar.Collapse>
         </Navbar>
