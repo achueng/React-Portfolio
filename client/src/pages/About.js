8c37cc29d5decabc8e1c4d35fe0ce3ee6d6d9aff
Agnes Chueng <agneschueng@gmail.com>
2020-11-26 16:28:32 -0600

    input components on About page

---
+++ client/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+import AboutMe from "../components/AboutMe";
+
+function About() {
+    return (
+        <Layout>
+            <AboutMe/>
+        </Layout>
+    )
+}
+
+export default About;
