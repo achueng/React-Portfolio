f0d29a79f76b69f48537de4fb0e3efae67de16c0
Agnes Chueng <agneschueng@gmail.com>
2020-11-26 16:27:19 -0600

    utilize react-bootstrap grid components instead of bootstrap class names

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 import "./css/Footer.css";
 
 function Footer(props) {
   return (
-    <footer className="row fixed-bottom p-3">
-        <h6 className="col-lg-12 col-md-12 col-sm-12"> &#169; Copyright 2020 Agnes Chueng</h6>
-        <a className="credit" href="https://www.freepik.com/free-vector/watercolor-design-pastel-background_7335470.htm">Background image designed by Freepik</a>
+    <footer className="fixed-bottom p-3">
+      <Row>
+        <Col>
+          <h6> &#169; Copyright 2020 Agnes Chueng</h6>
+          <a className="credit" href="https://www.freepik.com/free-vector/watercolor-design-pastel-background_7335470.htm">Background image designed by Freepik</a>
+        </Col>
+      </Row>
     </footer>
   )
 }
