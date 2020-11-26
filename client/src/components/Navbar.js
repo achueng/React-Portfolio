68069add7dd42e575229c90d712966a59b85f6b9
Agnes Chueng <agneschueng@gmail.com>
2020-11-25 18:28:47 -0600

    update navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,27 @@
-function Navbar() {
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import "./css/NavBar.css";
+
+function NavBar() {
     return (
-        <Navbar bg="light" expand="lg" fixed="top">
-        <Navbar.Brand to="/">Agnes Chueng</Navbar.Brand>
+        <Navbar expand="lg" fixed="top" className="custom-navbar">
+        <Navbar.Brand href="/" className="custom-brand">Agnes Chueng</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-                <Nav.Link to="/about"
+                <Nav.Link href="/about"
                 className={
                     window.location.pathname === "/" || 
                     window.location.pathname === "/about"
                       ? "active" : ""
                 }
                 >About</Nav.Link>
-                <Nav.Link to="/portfolio" className={window.location.pathname === "/portfolio" ? "active" : ""}>Portfolio</Nav.Link>
-                <Nav.Link to="/contact" className={window.location.pathname === "/contact" ? "active" : ""}>Contact</Nav.Link>
+                <Nav.Link href="/portfolio" className={window.location.pathname === "/portfolio" ? "active" : ""}>Portfolio</Nav.Link>
+                <Nav.Link href="/contact" className={window.location.pathname === "/contact" ? "active" : ""}>Contact</Nav.Link>
             </Nav>
         </Navbar.Collapse>
         </Navbar>
     )
 }
 
-export default Navbar;
+export default NavBar;
