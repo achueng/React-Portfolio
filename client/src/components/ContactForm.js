44ce99032c83ca7693cf2f0049bc0d4263909506
Agnes Chueng <agneschueng@gmail.com>
2020-11-28 00:16:12 -0600

    fixed class to be className

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -5,21 +5,21 @@ import { Form, Button } from "react-bootstrap";
 function ContactForm() {
     return (
         <React.Fragment>
-            <h1 class="display-4">Contact</h1>
-            <hr class="my-4"></hr>
+            <h1 className="display-4">Contact</h1>
+            <hr className="my-4"></hr>
             <Form>
-                <Form.Group controlId="formBasicName">
+                <Form.Group controlId="formName">
                     <Form.Label>Name</Form.Label>
                     <Form.Control type="text" placeholder="Name" />
                 </Form.Group>
-                <Form.Group controlId="formBasicEmail">
+                <Form.Group controlId="formEmail">
                     <Form.Label>Email</Form.Label>
                     <Form.Control type="email" placeholder="Email" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
-                <Form.Group controlId="formBasicTextArea">
+                <Form.Group controlId="formMessage">
                     <Form.Label>Message</Form.Label>
                     <Form.Control as="textarea" rows={3} placeholder="Enter message here"/>
                 </Form.Group>
