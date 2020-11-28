a9e84270480a0d3f8a4da0fef95a0400cd661f90
Agnes Chueng <agneschueng@gmail.com>
2020-11-27 23:44:24 -0600

    add Route components to each page

--- client/src/App.js
+++ client/src/App.js
@@ -12,10 +12,10 @@ function App() {
     <Router>
       <NavBar/>
       <Wrapper>
-        {/* <Route exact path="/" component={About}/>
+        <Route exact path="/" component={About}/>
         <Route exact path="/about" component={About}/>
         <Route exact path="/portfolio" component={Portfolio}/>
-        <Route exact path="/contact" component={Contact}/> */}
+        <Route exact path="/contact" component={Contact}/>
       </Wrapper>
       <Footer/>
     </Router>
