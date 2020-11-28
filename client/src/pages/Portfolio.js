8c5a28ab77a08cd5a7c062e8629978c69e522a76
Agnes Chueng <agneschueng@gmail.com>
2020-11-27 23:31:53 -0600

    add layout and portfolio components

---
+++ client/src/pages/Portfolio.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+import PortfolioCards from "../components/PortfolioCards";
+
+function Portfolio() {
+    return (
+        <Layout>
+            <PortfolioCards/>
+        </Layout>
+    )
+}
+
+export default Portfolio;
