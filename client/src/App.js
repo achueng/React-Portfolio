c39bb458a8c0b491aa3946fe2dd144678444ea58
Agnes Chueng <agneschueng@gmail.com>
2020-12-01 18:54:58 -0600

    remove navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
 import Wrapper from "./components/Wrapper";
 import Footer from "./components/Footer";
 import About from "./pages/About";
@@ -10,7 +9,6 @@ import Contact from "./pages/Contact";
 function App() {
   return (
     <Router>
-      <NavBar/>
       <Wrapper>
         <Route exact path="/" component={About}/>
         <Route exact path="/about" component={About}/>
