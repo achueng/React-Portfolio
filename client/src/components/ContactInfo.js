5bc910821ea1fef9d0ed8cc1f9ac7377145a9f68
Agnes Chueng <agneschueng@gmail.com>
2020-12-02 19:57:33 -0600

    add href to anchor tags

--- client/src/components/ContactInfo.js
+++ client/src/components/ContactInfo.js
@@ -7,18 +7,18 @@ function ContactInfo() {
         <Card className="py-3 sticky-top contact-card">
             <Row>
                 <Col xs={4} md={12}>
-                    <a href="" alt="">
-                        <Image src="https://picsum.photos/80/80" alt="" roundedCircle className="icons my-2 mx-auto"/>
+                    <a href="https://github.com/achueng" alt="GitHub" target="_blank">
+                        <Image src="https://picsum.photos/80/80" alt="github-logo" roundedCircle className="icons my-2 mx-auto"/>
                     </a>
                 </Col>
-                <Col xs={4} md={12} className="justify-content-center">
-                    <a href="" alt="">
-                        <Image src="https://picsum.photos/80/80" alt="" roundedCircle className="icons my-2 mx-auto"/>
+                <Col xs={4} md={12} className="justify-content-center" target="_blank">
+                    <a href="https://www.linkedin.com/in/agnes-chueng-5872659b/" alt="LinkedIn">
+                        <Image src="https://picsum.photos/80/80" alt="linkedin-logo" roundedCircle className="icons my-2 mx-auto"/>
                     </a>
                 </Col>
                 <Col xs={4} md={12}>
-                    <a href="" alt="">
-                        <Image src="https://picsum.photos/80/80" alt="" roundedCircle className="icons my-2 mx-auto"/>
+                    <a href="mailto:agneschueng2020@u.northwestern.edu" alt="Email" target="_blank">
+                        <Image src="https://picsum.photos/80/80" alt="mail-logo" roundedCircle className="icons my-2 mx-auto"/>
                     </a>
                 </Col>
             </Row>
