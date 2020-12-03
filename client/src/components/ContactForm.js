4768cfdf503aa975e2e13840af74e44030d696cc
Agnes Chueng <agneschueng@gmail.com>
2020-12-02 22:53:15 -0600

    pass in props as onChange and onSubmit for form components

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./css/ContactForm.css";
 import { Form, Button } from "react-bootstrap";
 
-function ContactForm() {
+function ContactForm(props) {
     return (
         <React.Fragment>
             <h1 className="display-4">Contact</h1>
@@ -10,20 +10,40 @@ function ContactForm() {
             <Form>
                 <Form.Group controlId="formName">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Name" />
+                    <Form.Control 
+                    type="text"
+                    onChange={props.handleInputChange}
+                    placeholder="Name"
+                    name="name"
+                    />
                 </Form.Group>
                 <Form.Group controlId="formEmail">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Email" />
+                    <Form.Control
+                    type="email"
+                    onChange={props.handleInputChange}
+                    placeholder="Email"
+                    name="email"
+                    />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
                 <Form.Group controlId="formMessage">
                     <Form.Label>Message</Form.Label>
-                    <Form.Control as="textarea" rows={3} placeholder="Enter message here"/>
+                    <Form.Control 
+                    as="textarea"
+                    rows={3}
+                    onChange={props.handleInputChange}
+                    placeholder="Enter message here"
+                    name="message"
+                    />
                 </Form.Group>
-                <Button variant="light" type="submit">
+                <Button 
+                variant="light" 
+                type="submit"
+                onClick = {props.handleFormSubmit}
+                >
                     Submit
                 </Button>
             </Form>
