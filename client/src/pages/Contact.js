d7851477ab61902e82d8925c1a3ace0347083188
Agnes Chueng <agneschueng@gmail.com>
2020-12-02 22:51:44 -0600

    create state and functions to handle change & submit

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 import ContactForm from "../components/ContactForm";
+import API from "../utils/API";
 
 function Contact() {
+    const [formState, setFormState] = useState({})
+
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setFormState({...formState, [name]: value})
+    }
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        API.postContacts(formState).then(res => {
+            setFormState(formState)
+        }).catch(err => console.log(err));
+    }
+
     return (
         <Layout>
-            <ContactForm/>
+            <ContactForm
+            handleInputChange = {handleInputChange}
+            handleFormSubmit = {handleFormSubmit}
+            />
         </Layout>
     )
 }
