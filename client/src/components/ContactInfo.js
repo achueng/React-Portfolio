224be4e04d6c8d5688c5c9db1e5a7c814be333a4
Agnes Chueng <agneschueng@gmail.com>
2020-12-02 20:01:57 -0600

    link image src to Image tags

--- client/src/components/ContactInfo.js
+++ client/src/components/ContactInfo.js
@@ -8,17 +8,17 @@ function ContactInfo() {
             <Row>
                 <Col xs={4} md={12}>
                     <a href="https://github.com/achueng" alt="GitHub" target="_blank">
-                        <Image src="https://picsum.photos/80/80" alt="github-logo" roundedCircle className="icons my-2 mx-auto"/>
+                        <Image src={process.env.PUBLIC_URL + "/assets/github-logo.png"} alt="github-logo" rounded className="icons my-2 mx-auto"/>
                     </a>
                 </Col>
                 <Col xs={4} md={12} className="justify-content-center" target="_blank">
                     <a href="https://www.linkedin.com/in/agnes-chueng-5872659b/" alt="LinkedIn">
-                        <Image src="https://picsum.photos/80/80" alt="linkedin-logo" roundedCircle className="icons my-2 mx-auto"/>
+                        <Image src={process.env.PUBLIC_URL + "/assets/linkedin-logo.png"} alt="linkedin-logo" rounded className="icons my-2 mx-auto"/>
                     </a>
                 </Col>
                 <Col xs={4} md={12}>
                     <a href="mailto:agneschueng2020@u.northwestern.edu" alt="Email" target="_blank">
-                        <Image src="https://picsum.photos/80/80" alt="mail-logo" roundedCircle className="icons my-2 mx-auto"/>
+                        <Image src={process.env.PUBLIC_URL + "/assets/mail-logo.png"} alt="mail-logo" rounded className="icons my-2 mx-auto"/>
                     </a>
                 </Col>
             </Row>
