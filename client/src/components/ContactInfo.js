3fddede5e3ef0f93610a80d261d4667582263282
Agnes Chueng <agneschueng@gmail.com>
2020-12-02 20:06:34 -0600

    add rel for anchor tags

--- client/src/components/ContactInfo.js
+++ client/src/components/ContactInfo.js
@@ -7,17 +7,17 @@ function ContactInfo() {
         <Card className="py-3 sticky-top contact-card">
             <Row>
                 <Col xs={4} md={12}>
-                    <a href="https://github.com/achueng" alt="GitHub" target="_blank">
+                    <a href="https://github.com/achueng" alt="GitHub" target="_blank" rel="noreferrer">
                         <Image src={process.env.PUBLIC_URL + "/assets/github-logo.png"} alt="github-logo" rounded className="icons my-2 mx-auto"/>
                     </a>
                 </Col>
-                <Col xs={4} md={12} className="justify-content-center" target="_blank">
+                <Col xs={4} md={12} className="justify-content-center" target="_blank" rel="noreferrer">
                     <a href="https://www.linkedin.com/in/agnes-chueng-5872659b/" alt="LinkedIn">
                         <Image src={process.env.PUBLIC_URL + "/assets/linkedin-logo.png"} alt="linkedin-logo" rounded className="icons my-2 mx-auto"/>
                     </a>
                 </Col>
                 <Col xs={4} md={12}>
-                    <a href="mailto:agneschueng2020@u.northwestern.edu" alt="Email" target="_blank">
+                    <a href="mailto:agneschueng2020@u.northwestern.edu" alt="Email" target="_blank" rel="noreferrer">
                         <Image src={process.env.PUBLIC_URL + "/assets/mail-logo.png"} alt="mail-logo" rounded className="icons my-2 mx-auto"/>
                     </a>
                 </Col>
